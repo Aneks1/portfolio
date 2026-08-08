017f0e1b3dd322775315c80ff6457fe07f7f9e6a
Aneks1 <proames75yt@gmail.com>
2026-08-08 12:06:32 -0500

    Improved SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  integrations: [vue(), sitemap()],
   output: 'server',
   adapter: vercel(),
 });
-
